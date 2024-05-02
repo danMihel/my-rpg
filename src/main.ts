3173a29f6b40d883e503708a61b3f4ec1a7484dd
danMihel <d.mihel78@gmail.com>
2024-05-02 12:24:46 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ new Phaser.Game({
   title: 'Phaser RPG',
   scene: scenes,
   url: import.meta.env.URL || '',
-  version: import.meta.env.URL || '0.0.1',
+  version: import.meta.env.URL || '0.0.01',
   backgroundColor: '#000',
   scale: {
     mode: Phaser.Scale.FIT,
